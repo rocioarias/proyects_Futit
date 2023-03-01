9bae67ff2990a99eee9d381698098e4cd2a2a2d1
Rocio A <101805934+rocioarias@users.noreply.github.com>
2023-03-01 14:46:00 -0300

    Odd square done, is in work the even.

--- java/magicSquare.java
+++ java/magicSquare.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package java;
 
 public class magicSquare {
@@ -10,38 +9,64 @@ public class magicSquare {
     public magicSquare(int size) {
         this.size = size;
         this.magicNumber = (int) (((size)*(Math.pow(size, 2)+1))/2);
-        this.result = fullZeros(new int[size][size]);
+        this.result = new int[size][size];
         fillSquareOdd(size, result);
-        /*Working in a the other 3 options = Odd 2, multiples of 4 and multiples of 4 + 2 */
+        /*Working in a the other options = Odd 2, multiples of 4 and multiples of 4 + 2 */
     }
 
 /* ---------------------------------------- Methods ---------------------------------------------- */
-    private int[][] fullZeros(int[][] square){
-        for(int i = 0; i < square.length; i++){
-            for(int j = 0; j < square.length; j++){
-                square[i][j] = 0;
-            }
-        }
-        return square;
+    /* Case 1 being n odd with the Siamese method. */
+    public void fillSquareOdd(int s, int[][]square){
+
+            /* 
+             * We have to initializate the rows and the columns, being the first one
+             * in the last position, and the column in the middle. 
+             * After doing that, the square is gonna have the number one in that specific coordinates
+             * as an start.
+             */
+            int r = s-1;
+            int c = s/2;
+            square[r][c] = 1;
+            
+            /*
+             * There's two options:
+             * 1. If this place is empty, we go the row under the actual. After doing that we go to the column
+             * next to the current one.
+             * 2. If this place is NOT empty, we break the diagonal and go to the one thats up the current one without modifications
+             * in the column.
+             */
+            for (int i = 2; i <= s*s; i++) {
+                /*
+                 * To know if is empty or not, we have to verify if the spot has a zero, because
+                 * automatically is gonna be full of zeros, so that's gonna be our parameter.
+                 */
+                if (square[(r+1)%s][(c+1)%s] == 0) {
+                    r = (r+1)%s;
+                    c = (c+1)%s;
+                } else {
+                    r = (r-1+s)%s;
+                }
+
+                /*
+                 * When we have the place correctly, is time to put the number in the square.
+                 */
+
+                square[r][c] = i;
+            }        
     }
 
-    public void fillSquareOdd(int s, int[][]square){
-        int r = s-1;
-        int c = s/2;
-    
-        square[r][c] = 1;
-
-        for(int i=2; i <= s*s; i++){
-            int verification = square[(r+1)%s][(c+1) %s];
-            if (verification == 0) {
-                r = (r+1) % s;
-                c = (c+1) % s;
-            }
-            else {
-                r = (r-1+s) % s;
-            }
-            square[r][c] = i;
+    public void fillSquareMultipliesOfFour(int s, int[][] square) {
+        /* Fill the square with diagonals*/
+        int num = 1, j=0, i=0;
+        for(i = 0; i < s; i++){
+            for(j = 0; j < s; j++){
+                square[i][j] = num;
+                num++; 
+            } 
         }
+
+
+
     }
 
     @Override
@@ -98,113 +123,8 @@ public class magicSquare {
 /* ---------------------------------------------------------------------------------- */    
 
     public static void main(String[] args) {
-        magicSquare Cuadrado = new magicSquare(4);
+        magicSquare Cuadrado = new magicSquare(6);
         System.out.println(Cuadrado.toString());
     }
 
-=======
-package java;
-
-public class magicSquare {
-
-    private int size;
-    private int magicNumber;
-    private int[][] result;
-
-    public magicSquare(int size) {
-        this.size = size;
-        this.magicNumber = (int) (((size)*(Math.pow(size, 2)+1))/2);
-        this.result = fullZeros(new int[size][size]);
-        fillSquareOdd(size, result);
-        /*Working in a the other 3 options = Odd 2, multiples of 4 and multiples of 4 + 2 */
-    }
-
-/* ---------------------------------------- Methods ---------------------------------------------- */
-    private int[][] fullZeros(int[][] square){
-        for(int i = 0; i < square.length; i++){
-            for(int j = 0; j < square.length; j++){
-                square[i][j] = 0;
-            }
-        }
-        return square;
-    }
-
-    public void fillSquareOdd(int s, int[][]square){
-        int r = s-1;
-        int c = s/2;
-    
-        square[r][c] = 1;
-
-        for(int i=2; i <= s*s; i++){
-            int verification = square[(r+1)%s][(c+1) %s];
-            if (verification == 0) {
-                r = (r+1) % s;
-                c = (c+1) % s;
-            }
-            else {
-                r = (r-1+s) % s;
-            }
-            square[r][c] = i;
-        }
-    }
-
-    @Override
-    public String toString() {
-        String auxiliarString = "";
-        for(int i = 0; i < result.length; i++){
-            for(int j = 0; j < result.length; j++){
-                if(j==0){
-                    auxiliarString += "\n" + result[i][j];
-                }
-                else{
-                    auxiliarString += " " + result[i][j];
-                }
-            }
-        }
-        return auxiliarString;
-    }
-
-/* --------------------------------- Setters and Getters ------------------------------------- */    
-    public int getSize() {
-        return size;
-    }
-
-    /* 
-     * When you are setting the size, at the same time you are setting a new 
-     * magic number, that two numbers has to be relationated.  
-    */
-
-    public void setSize(int size) {
-        this.size = size;
-        this.magicNumber = (int) (((size)*(Math.pow(size, 2)+1))/2);
-    }
-
-    /* 
-     * This has to be stricly relationated with the size, you can't change the magic number directly, 
-     * the size is determinating the magic number.
-     */
-    public void setMagicNumber(int magicNumber) {
-        throw new RuntimeException("To change the magic number, change the size of the magic square");
-    }
-
-    public int getMagicNumber() {
-        return magicNumber;
-    }
-
-    public int[][] getResult() {
-        return result;
-    }
-
-
-    public void setResult(int[][] result) {
-        this.result = result;
-    }
-/* ---------------------------------------------------------------------------------- */    
-
-    public static void main(String[] args) {
-        magicSquare Cuadrado = new magicSquare(4);
-        System.out.println(Cuadrado.toString());
-    }
-
->>>>>>> 5d9892c2c28066a8316d735147db220b6b5a4f0f
 }
